0164737cbf73c93a17799fce9359e9500501a9e8
unknown <muharsyad2201@gmail.com>
2025-01-22 11:13:17 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function toggleNavbar() {
+    const navLinks = document.getElementById('navLinks');
+    if (navLinks.style.display === "flex") {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'flex';
+    }
+}
